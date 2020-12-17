34ed65206c16144f91acaeb3f2caaba5f8f274d3
panu2306 <pranavbhendawade@gmail.com>
2020-12-17 11:15:36 +0530

    program to return even and odd numbered lists from main list

---
+++ gfg_ds_course/lists/even_odd_list.py
@@ -0,0 +1,19 @@
+def using_simple_method(l):
+    l1 = []
+    l2 = []
+    for num in l:
+        if(num%2 == 0):
+            l1.append(num)
+        else:
+            l2.append(num)
+    return l1, l2 
+
+def using_list_comprehension(l):
+    l1 = [num for num in l if(num%2 == 0)]
+    l2 = [num for num in l if(num%2 != 0)]
+    return l1, l2
+
+l = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print('Using `LIST-COMPREHENSION`: Even list from main list: {} and odd list from main list: {}'.format(using_list_comprehension(l)[0],using_list_comprehension(l)[1]))
+print('Using `SIMPLE METHOD`: Even list from main list: {} and odd list from main list: {}'.format(using_simple_method(l)[0], using_simple_method(l)[0]))
