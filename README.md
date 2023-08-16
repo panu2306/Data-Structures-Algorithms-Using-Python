8dbff17eed7b4e188778fca8882e09ba6f9ee0eb
panu2306 <pranavbhendawade@gmail.com>
2023-08-16 12:15:37 -0700

    efficient solution to contains_duplicate

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Data-Structures-Algorithms-Using-Python
 This repository contains all the algorithms of data structures implemented in python language. 
+
+## Arrays(List), HashMaps(Dictionary), and HashSets(Set) - 
+- Article on set - https://www.geeksforgeeks.org/sets-in-python/#
+
--- leetcode/arrays_hashing/contains_duplicate.py
+++ leetcode/arrays_hashing/contains_duplicate.py
@@ -6,6 +6,9 @@ def contains_duplicate_first(nums):
     outer loop keeps track of element that is being
     compared with the rest of elements by the 
     second loop which iterates through whole list.
+    
+    Time Complexity - O(n^2)
+    Space Complexity - O(1)
     """
     if(len(nums) == 1 or len(nums) == 0):
         return False
@@ -20,6 +23,9 @@ def contains_duplicate_second(nums):
     Using this approach, the list is first sorted. 
     Then, the two index variables which iterate
     over the list to find if they are duplicate.
+
+    Time Complexity - O(nlogn)
+    Space Complexity - O(1)
     """
     if(len(nums) == 1 or len(nums) == 0):
         return False
@@ -37,7 +43,10 @@ def contains_duplicate_third(nums):
     then using set function, which stores all unique
     values, the length of the set is compared with 
     length of list. This works since the set only 
-    stores the unique elements. 
+    stores the unique elements.
+
+    Time Complexity - O(nlogn)
+    Space Complexity - O(1)
     """
     if(len(nums) == 1 or len(nums) == 0):
         return False
@@ -47,6 +56,23 @@ def contains_duplicate_third(nums):
     else:
         return False
 
+def contains_duplicate_fourth(nums):
+    """
+    In this approach, a set is defined and
+    the elements are being added from the given 
+    list to the set. Since, set only adds unique
+    elements, it will inform if element being
+    added is already present in set
+    
+    Time Complexity - O(n)
+    Space Complexity - O(n)
+    """
+    hashset = set()
+    for num in nums: 
+        if num in hashset: 
+            return True
+        hashset.add(num)
+    return False
 
 # True Case: 
 nums = [1,1,1,3,3,4,3,2,4,2]
@@ -56,3 +82,4 @@ nums = [1,1,1,3,3,4,3,2,4,2]
 print(contains_duplicate_first(nums))
 print(contains_duplicate_second(nums))
 print(contains_duplicate_third(nums))
+print(contains_duplicate_fourth(nums))
